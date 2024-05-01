a69e9848b5f02cd3dc65f4527cc4565320d96ed6
KoPlayz <koplayzmail@pm.me>
2024-05-01 15:52:56 -0400

    Fixed install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-echo Script by Github/KoPlayz for UEFI systems (v1.1-2024-04-12).
+echo "Script by Github/KoPlayz for UEFI systems (v1.1-2024-04-12)."
 echo "You will need a partition for EFI (1GB Recommended), Root (majority), swap (RAM *2 recommended)"
 echo " "
 read -p "Confirm you have made these partitions, then type in the name of the partitions: (/dev/...)"
@@ -8,16 +8,19 @@ read -p "Confirm you have made these partitions, then type in the name of the pa
 read -p "EFI Partition: /dev/" efipart
 efipart="/dev/$efipart"
 echo "EFI (/boot) Partition is set to: $efipart"
+echo "------------------------------------------------"
 
 #rootpart=userinput
 read -p "Root Partition: /dev/" rootpart
 rootpart="/dev/$rootpart"
 echo "Root (/) Partition is set to: $rootpart"
+echo "------------------------------------------------"
 
 #swappart=userinput
 read -p "Swap Partition: /dev/" swappart
 swappart="/dev/$swappart"
 echo "SWAP Partition is set to: $swappart"
+echo "------------------------------------------------"
 
 # format ext4
 mkfs.fat -F 32 $efipart
@@ -29,8 +32,9 @@ mntlocation=/mnt/karchinstall
 mount $rootpart $mntlocation
 mount --mkdir $efipart "$mntlocation/boot"
 swapon $swappart
+echo "------------------------------------------------"
 
-read -p "Add packages seperates by spaces ( ) (WHICH YOU KNOW EXIST!) that you want added in addition to the base packages" extrapackages
+read -p "Add packages seperates by spaces ( ) (WHICH YOU KNOW EXIST!) that you want added in addition to the base packages: " extrapackages
 # Asking if cpu is AMD or Intel for microcode, then converting to lowercase
 read -p "Is your CPU an AMD or Intel? " cpubrand1
 cpubrand="${cpubrand1,,}"
@@ -42,17 +46,18 @@ if [[ $confirm != "n" ]]; then
 else
     echo "Please re-enter the CPU brand."
 fi
+echo "------------------------------------------------"
 # Pacstrap installing packages to /mnt
 cpubrand2="$cpubrand"
-echo Here, we install
+echo "Here, we install:"
 pacstrap -K $mntlocation base linux linux-firmware reflector nano grub $cpubrand2-ucode vim man-db man-pages texinfo networkmanager
 
-genfstab -U $karchinstall >> $karchinstall/etc/fstab
-
+genfstab -U $mntlocation >> $mntlocation/etc/fstab
+echo "------------------------------------------------"
 # Final touches
 read -p "What would you like to set your hostname to? " hostname
-echo "$hostname" > "$karchinstall/etc/hostname"
-arch-chroot $karchinstall /bin/bash <<EOF
+echo "$hostname" > "$mntlocation/etc/hostname"
+arch-chroot $mntlocation /bin/bash <<EOF
 # Create a new user and set password/root password
 read -p "What should the username be? " username
 read -p "What should the password be? " password
